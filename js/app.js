047be371879d93f1c857a11254f999dbfd0cf98f
邴新科-工作 <kylebing@163.com>
2023-02-06 11:51:26 +0800

    fix 内容复制

--- js/app.js
+++ js/app.js
@@ -16,17 +16,20 @@ let app = new Vue({
     mounted() {
         // 绑定剪贴板操作方法
         this.clipboard = new ClipboardJS('.clipboard', {
-            text: function (trigger) {
+            text:  (trigger) => {
                 return trigger.getAttribute('dataClipboard')
             },
+        })
+        this.clipboard.on('success', ()=>{  // 还可以添加监听事件，如：复制成功后提示
+            console.log('复制成功')
         })
     },
     methods: {
         stringToBase64(){
-            this.base64Data = btoa(this.stringData)
+            this.base64Data = Base64.encode(this.stringData)
         },
         base64ToString(){
-            this.stringData = atob(this.base64Data)
+            this.stringData = Base64.decode(this.base64Data)
         },
     },
     watch: {}
